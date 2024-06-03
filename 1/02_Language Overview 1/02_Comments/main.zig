72d1361a3de7ff0258c95d0b08061f904cae7c79
Danial Mobini <danialmobinidh81@gmail.com>
2024-06-03 13:07:17 +0330

    comments

---
+++ 1/02_Language Overview 1/02_Comments/main.zig
@@ -0,0 +1,5 @@
+// This code won't compile if `main` isn't `pub` (public)
+// Zig doesn't have multi-line comments, like C's /* ... */.
+// ! is known as a top-level document comment and can be placed at the top of the file.
+// A triple-slash comment (///), known as a document comment, can go in specific places such as before a declaration.
+// You'll get a compiler error if you try to use either type of document comment in the wrong place.
