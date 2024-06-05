c8f191d7fab5f1bd093af79e170455175ae3ac8d
Danial Mobini <danialmobinidh81@gmail.com>
2024-06-05 18:39:23 +0330

    switch

---
+++ 03_Language Overview 2/01_Control Flow/02_switch/main.zig
@@ -0,0 +1,31 @@
+// switch is similar to an if/else if/else, but has the advantage of
+// being exhaustive. That is, it's a compile-time error if not all cases
+// are covered. This code will not compile:
+fn anniversaryName(years_married: u16) []const u8 {
+    switch (years_married) {
+        1 => return "paper",
+        2 => return "cotton",
+        3 => return "leather",
+        4 => return "flower",
+        5 => return "wood",
+        6 => return "sugar",
+        //        Yes, but thankfully there's an else:
+        else => return "no more gifts for you",
+    }
+}
+
+// We can combine multiple cases or use ranges, and use blocks for complex cases:
+fn arrivalTimeDesc(minutes: u16, is_late: bool) []const u8 {
+    switch (minutes) {
+        0 => return "arrived",
+        1, 2 => return "soon",
+        3...5 => return "no more than 5 minutes",
+        else => {
+            if (!is_late) {
+                return "sorry, it'll be a while";
+            }
+            // todo, something is very wrong
+            return "never";
+        },
+    }
+}
