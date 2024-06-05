79c2891192dd7feb024fd9da310828c7b97e1620
Danial Mobini <danialmobinidh81@gmail.com>
2024-06-05 19:02:21 +0330

    ifs

--- 03_Language Overview 2/01_Control Flow/01_ifs/main.zig
+++ 03_Language Overview 2/01_Control Flow/01_ifs/main.zig
@@ -1,15 +1,20 @@
+const std = @import("std");
 // Zig's if, else if and else are commonplace:
+const method: []const u8 = "GET";
+fn func1() void {
 
-// std.mem.eql does a byte-by-byte comparison
-// for a string it'll be case sensitive
-if (std.mem.eql(u8, method, "GET") or std.mem.eql(u8, method, "HEAD")) {
-// handle a GET request
-} else if (std.mem.eql(u8, method, "POST")) {
-// handle a POST request
-} else {
-// ...
+    // std.mem.eql does a byte-by-byte comparison
+    // for a string it'll be case sensitive
+    if (std.mem.eql(u8, method, "GET") or std.mem.eql(u8, method, "HEAD")) {
+        // handle a GET request
+    } else if (std.mem.eql(u8, method, "POST")) {
+        // handle a POST request
+    } else {
+        // ...
+    }
 }
 
+var power: i64 = 8000;
 // The above example is comparing ASCII strings and should likely be case
 // insensitive. std.ascii.eqlIgnoreCase(str1, str2) is probably a better option.
 // There is no ternary operator, but you can use an if/else like so:
